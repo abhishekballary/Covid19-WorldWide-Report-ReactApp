f2920088e3ea3fbd584f059356c8789705bd7bea
abhishekballary <abhishekballery@gmail.com>
2021-08-30 20:33:02 +0530

    build

---
+++ build/precache-manifest.966597e23721de0fc8956f6d8af22968.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "711d98116c6410425f847092e0e1c0b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63cd27d225495ef11f02",
+    "url": "/static/css/main.ae6ad66a.chunk.css"
+  },
+  {
+    "revision": "04636cbb98146b24616d",
+    "url": "/static/js/2.c31a4ff6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c31a4ff6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63cd27d225495ef11f02",
+    "url": "/static/js/main.d763ba55.chunk.js"
+  },
+  {
+    "revision": "d5a837d1dfe1a72c868e",
+    "url": "/static/js/runtime-main.65a02a3f.js"
+  },
+  {
+    "revision": "ed4e6f6897234e781d38209617fc16ea",
+    "url": "/static/media/covid-virus.ed4e6f68.png"
+  },
+  {
+    "revision": "1c348a92d30afe8dc93f9c1ec0f99f6a",
+    "url": "/static/media/spinner.1c348a92.gif"
+  }
+]);
